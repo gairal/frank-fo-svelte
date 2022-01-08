9bd594c97fc27ae6a51409439acfc0fec346befc
frank.gairal <frank@gairal.com>
2022-01-08 13:02:30 +0100

    fix: lint

--- src/components/Header/Link.svelte
+++ src/components/Header/Link.svelte
@@ -4,11 +4,11 @@
 
   export let to: string;
 
-  const handleGetProps = ({ isCurrent }: GetPropsParams) => ({
+  const getProps = ({ isCurrent }: GetPropsParams) => ({
     class: `flex justify-center item-center p-3 rounded-xl bg-purple-900 nav-link${
       isCurrent ? " nav-link--active" : ""
     }`,
   });
 </script>
 
-<Link {to} getProps={handleGetProps}><slot /></Link>
+<Link {to} {getProps}><slot /></Link>
