3ea5c6b8e85c99d6ae4d9b6d434f664ccc0fcaf8
frank.gairal <frank@gairal.com>
2022-01-08 02:09:09 +0100

    feat: trying to use sass

---
+++ src/components/Header/Menu.svelte
@@ -0,0 +1,11 @@
+<script>
+  import MenuIcon from "../icons/MenuIcon.svelte";
+</script>
+
+<button
+  aria-label="more"
+  class="p-3 rounded-full hover:bg-purple-900 md:hidden"
+  type="button"
+>
+  <MenuIcon />
+</button>
--- src/components/Header/index.svelte
+++ src/components/Header/index.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
   import Link from "./Link.svelte";
+  import Menu from "./Menu.svelte";
   import More from "./More.svelte";
 </script>
 
 <header
-  class="w-full h-24 rounded-2xl mx-auto mb-8 flex items-center px-8 text-white border-4 border-purple-200 font-cursive border-neon"
+  class="w-full h-24 rounded-2xl mx-auto mb-8 flex items-center px-4 text-white border-4 border-purple-200 font-cursive border-neon"
 >
   <h1 class="text-5xl flex-1 uppercase text-neon">work</h1>
-  <nav class="flex space-x-4 mr-2">
+  <nav class="flex flex-col md:flex-row space-x-4 mr-2">
     <Link to="/">Work</Link>
     <Link to="education">Education</Link>
-    <Link to="skill">Skills</Link>
+    <Link to="skills">Skills</Link>
     <Link to="interests">Interests</Link>
   </nav>
+  <Menu />
   <More />
 </header>
