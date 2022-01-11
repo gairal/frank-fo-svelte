d24471bceae2a948fe7da72f48e75ba94e03f6bb
frank.gairal <frank@gairal.com>
2022-01-11 04:05:42 +0100

    fix: style

--- src/components/Header/index.svelte
+++ src/components/Header/index.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import Menu from "./Menu.svelte";
   import More from "./More.svelte";
-  import { title } from "../../stores";
   import Nav from "./Nav.svelte";
 
   let expanded = false;
@@ -12,13 +11,12 @@
 </script>
 
 <header
-  class="relative w-full rounded-2xl mx-auto mb-8 px-4 text-white border-4 border-purple-200 font-cursive border-neon"
+  class="relative w-full rounded-2xl mx-auto px-4 text-white border-4 border-purple-200 font-cursive border-neon"
 >
-  <span class="absolute left-10 text-neon text-purple-200 bg-purple-900"
-    >frank g.</span
-  >
-  <div class="h-24 flex items-center">
-    <h1 class="text-3xl md:text-4xl flex-1 uppercase text-neon">{$title}</h1>
+  <div class="h-20 flex items-center">
+    <h1 class="text-3xl md:text-4xl flex-1 text-purple-200 text-neon">
+      frank g.
+    </h1>
     <Nav class="hidden md:flex space-x-4" />
     <Menu {handleClick} />
     <More />
@@ -27,9 +25,3 @@
     <Nav class="flex flex-col md:hidden space-y-4" />
   {/if}
 </header>
-
-<style>
-  span {
-    top: -1rem;
-  }
-</style>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -5,8 +5,14 @@
   import Interests from "./Interests/index.svelte";
   import Skills from "./Skills/index.svelte";
   import Work from "./Work/index.svelte";
+  import { title } from "../stores";
 </script>
 
+<h2
+  class="flex justify-center text-2xl md:text-3xl text-neon font-cursive uppercase text-white"
+>
+  {$title}
+</h2>
 <main
   class="flex-1 w-full rounded-2xl font-mono text-white mx-auto p-4 overflow-y-auto border-2 border-purple-200 border-neon"
 >
