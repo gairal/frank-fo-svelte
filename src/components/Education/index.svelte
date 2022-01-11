0f39d52ea659fb3861b97deb5e97294face39ecd
frank.gairal <frank@gairal.com>
2022-01-11 02:31:53 +0100

    fix: add progress

--- src/components/Education/index.svelte
+++ src/components/Education/index.svelte
@@ -25,4 +25,6 @@
   <div>
     {edu.name}
   </div>
+{:else}
+  <progress />
 {/each}
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const API_URL = "https://api-dot-com-gairal-frank-22.ue.r.appspot.com/";
+// export const API_URL = "http://localhost:8080/";
