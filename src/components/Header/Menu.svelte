6856fc9a96cc785520255d8741840f6223ea03a9
frank.gairal <frank@gairal.com>
2022-01-12 12:14:55 +0100

    fix: cleanup

--- src/components/Header/Menu.svelte
+++ src/components/Header/Menu.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
   import MenuIcon from "../icons/MenuIcon.svelte";
 
-  export let onClick: () => void;
+  const dispatch = createEventDispatcher();
 </script>
 
 <button
   aria-label="more"
   class="p-3 rounded-full hover:bg-purple-900 md:hidden"
-  on:click={onClick}
+  on:click={() => dispatch("click")}
   type="button"
 >
   <MenuIcon />
--- src/components/Header/index.svelte
+++ src/components/Header/index.svelte
@@ -18,10 +18,10 @@
       frank g.
     </h1>
     <Nav class="hidden md:flex space-x-4 z-20" />
-    <Menu onClick={handleClick} />
+    <Menu on:click={handleClick} />
     <More />
   </div>
   {#if expanded}
-    <Nav onClick={handleClick} class="flex flex-col md:hidden space-y-4" />
+    <Nav on:click={handleClick} class="flex flex-col md:hidden space-y-4" />
   {/if}
 </header>
