775f1850be14555e2dc2cd1509641e6387b10c37
frank.gairal <frank@gairal.com>
2022-01-12 04:05:21 +0100

    fix: new test

--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -1,23 +1,45 @@
 import { render, screen } from "@testing-library/svelte";
+import userEvent from "@testing-library/user-event";
 import fetchMock from "jest-fetch-mock";
 
 import App from "../App.svelte";
 
-const subject = () => render(App);
+const subject = async () => {
+  render(App);
+
+  await screen.findByRole("heading", { name: "frank g." });
+};
 
 beforeEach(() => {
   fetchMock.mockResponse(JSON.stringify([]));
 });
 
 describe("App", () => {
-  test("routes to the right page", async () => {
-    subject();
+  test("displays default vue", async () => {
+    await subject();
 
     // header
-    await screen.findByText("frank g.");
     await screen.findByRole("link", { name: "Work" });
     await screen.findByRole("link", { name: "Education" });
     await screen.findByRole("link", { name: "Interests" });
     await screen.findByRole("link", { name: "Skills" });
+
+    await screen.findAllByRole("button", { name: "more" });
+
+    // default component loaded
+    await screen.findByRole("heading", { name: "Work" });
+  });
+
+  test("routes requests", async () => {
+    await subject();
+
+    userEvent.click(await screen.findByRole("link", { name: "Education" }));
+    await screen.findByRole("heading", { name: "Education" });
+
+    userEvent.click(await screen.findByRole("link", { name: "Skills" }));
+    await screen.findByRole("heading", { name: "Skills" });
+
+    userEvent.click(await screen.findByRole("link", { name: "Interests" }));
+    await screen.findByRole("heading", { name: "Interests" });
   });
 });
