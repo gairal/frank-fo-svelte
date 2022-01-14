1cc7ca954fe57b82ed04239d3701e9f41f038be3
frank.gairal <frank@gairal.com>
2022-01-14 11:58:25 +0100

    fix: style

--- src/components/Skills/index.svelte
+++ src/components/Skills/index.svelte
@@ -15,10 +15,14 @@
   });
 </script>
 
-<section class="p-4 pb-0 columns-1 md:columns-2 xl:columns-3">
-  {#each $skills as category (category.label)}
-    <Skill {category} />
+<section class="h-full p-4 pb-0">
+  {#if $skills.length}
+    <div class="columns-1 md:columns-2 xl:columns-3">
+      {#each $skills as category (category.label)}
+        <Skill {category} />
+      {/each}
+    </div>
   {:else}
     <Loader />
-  {/each}
+  {/if}
 </section>
