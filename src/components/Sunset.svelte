18169ebd559ce912b343dd03c5f4339f499487a1
frank.gairal <frank@gairal.com>
2022-01-14 18:12:21 +0100

    fix: sunset

--- src/components/Sunset.svelte
+++ src/components/Sunset.svelte
@@ -1,8 +1,53 @@
 <section
-  class="absolute bottom-0 h-32 w-64 sm:h-48 sm:w-96 md:h-64 md:w-[32rem] lg:h-96 lg:w-[48rem] xl:h-[32rem] xl:w-[64rem] overflow-hidden -z-10 inset-x-0 mx-auto opacity-60"
+  class="absolute bottom-0 h-32 w-64 sm:h-48 sm:w-96 md:h-64 md:w-[32rem] lg:h-96 lg:w-[48rem] xl:h-[32rem] xl:w-[64rem] overflow-hidden -z-10 inset-x-0 mx-auto opacity-40"
 >
-  <div
-    class="w-full bg-gradient-to-b from-yellow-300 via-red-500 to-red-900"
-    style="height: 200%; border-radius: 100% 100% 0 0"
-  />
+  <div class="w-full bg-gradient-to-b from-yellow-300 via-red-500 to-red-400" />
 </section>
+
+<style>
+  div {
+    height: 200%;
+
+    @apply absolute inset-0 content-none rounded-t-full;
+    mask-size: auto, auto 80%;
+    mask-position: top, bottom;
+    mask-image: linear-gradient(rgba(0, 0, 0, 1) 20.1%, rgba(0, 0, 0, 0) 20.1%),
+      linear-gradient(
+        to bottom,
+        rgba(0, 0, 0, 1) 0,
+        rgba(0, 0, 0, 1) 20px,
+        rgba(0, 0, 0, 0) 20px,
+        rgba(0, 0, 0, 0) 23px,
+        rgba(0, 0, 0, 1) 23px,
+        rgba(0, 0, 0, 1) 43px,
+        rgba(0, 0, 0, 0) 43px,
+        rgba(0, 0, 0, 0) 48px,
+        rgba(0, 0, 0, 1) 48px,
+        rgba(0, 0, 0, 1) 68px,
+        rgba(0, 0, 0, 0) 68px,
+        rgba(0, 0, 0, 0) 76px,
+        rgba(0, 0, 0, 1) 76px,
+        rgba(0, 0, 0, 1) 96px,
+        rgba(0, 0, 0, 0) 96px,
+        rgba(0, 0, 0, 0) 109px,
+        rgba(0, 0, 0, 1) 109px,
+        rgba(0, 0, 0, 1) 129px,
+        rgba(0, 0, 0, 0) 129px,
+        rgba(0, 0, 0, 0) 150px,
+        rgba(0, 0, 0, 1) 150px,
+        rgba(0, 0, 0, 1) 170px,
+        rgba(0, 0, 0, 0) 170px,
+        rgba(0, 0, 0, 0) 196px,
+        rgba(0, 0, 0, 1) 196px,
+        rgba(0, 0, 0, 1) 216px,
+        rgba(0, 0, 0, 0) 216px,
+        rgba(0, 0, 0, 0) 250px,
+        rgba(0, 0, 0, 1) 250px,
+        rgba(0, 0, 0, 1) 270px,
+        rgba(0, 0, 0, 0) 270px,
+        rgba(0, 0, 0, 0) 304px,
+        rgba(0, 0, 0, 1) 304px,
+        rgba(0, 0, 0, 1) 400px
+      );
+  }
+</style>
