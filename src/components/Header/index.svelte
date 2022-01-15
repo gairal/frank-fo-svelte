210e7a9b36426d624d2596338b7b781b245a1319
frank.gairal <frank@gairal.com>
2022-01-15 11:26:35 +0100

    fix: fonts

--- src/components/Header/index.svelte
+++ src/components/Header/index.svelte
@@ -14,12 +14,11 @@
   class="relative w-full rounded-2xl mx-auto px-4 mb-4 sm:mb-6 md:mb-8 text-white border-4 border-purple-200 font-cursive border-neon"
 >
   <div class="h-20 flex items-center">
-    <p
-      role="heading"
+    <banner
       class="text-2xl sm:text-3xl md:text-4xl flex-1 text-purple-200 text-neon"
     >
       frank g.
-    </p>
+    </banner>
     <Nav class="hidden md:flex space-x-4 z-20" />
     <Menu on:click={handleClick} />
     <More />
