a33310dc0cd2911bf715a520d17834e9e0408e3b
frank.gairal <frank@gairal.com>
2022-01-17 11:52:21 +0100

    fix: header link

--- src/components/Header/Nav.svelte
+++ src/components/Header/Nav.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { Router } from "svelte-routing";
   import { slide } from "svelte/transition";
   import { backOut } from "svelte/easing";
 
@@ -14,11 +13,9 @@
   on:click
   transition:slide={{ duration: 300, easing: backOut }}
 >
-  <Router>
-    <Link to="/">Work</Link>
-    <Link to="education">Education</Link>
-    <Link to="skills">Skills</Link>
-    <Link to="interests">Interests</Link>
-  </Router>
+  <Link to="/">Work</Link>
+  <Link to="education">Education</Link>
+  <Link to="skills">Skills</Link>
+  <Link to="interests">Interests</Link>
   <div />
 </nav>
--- src/components/Header/index.svelte
+++ src/components/Header/index.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { link, Router } from "svelte-routing";
+
   import Menu from "./Menu.svelte";
   import More from "./More.svelte";
   import Nav from "./Nav.svelte";
@@ -10,26 +12,28 @@
   };
 </script>
 
-<header
-  class="relative w-full rounded-2xl mx-auto px-4 mb-4 sm:mb-6 md:mb-8 text-white border-4 border-purple-200 font-cursive border-neon"
->
-  <div class="h-20 flex items-center">
-    <banner
-      class="text-2xl sm:text-3xl md:text-4xl flex-1 text-purple-200 text-neon"
-    >
-      frank g.
-    </banner>
-    <Nav class="hidden md:flex space-x-4 z-20" />
-    <Menu on:click={handleClick} />
-    <More />
-  </div>
-  {#if expanded}
-    <Nav on:click={handleClick} class="flex flex-col md:hidden space-y-4" />
-  {/if}
-</header>
+<Router>
+  <header
+    class="relative w-full rounded-2xl mx-auto px-4 mb-4 sm:mb-6 md:mb-8 text-white border-4 border-purple-200 font-cursive border-neon"
+  >
+    <div class="h-20 flex items-center">
+      <a
+        class="text-2xl sm:text-3xl md:text-4xl flex-1 text-purple-200 text-neon"
+        href="/"
+        use:link>frank g.</a
+      >
+      <Nav class="hidden md:flex space-x-4 z-20" />
+      <Menu on:click={handleClick} />
+      <More />
+    </div>
+    {#if expanded}
+      <Nav on:click={handleClick} class="flex flex-col md:hidden space-y-4" />
+    {/if}
+  </header>
+</Router>
 
 <style>
-  banner {
+  a {
     animation: flicker 1.5s infinite alternate;
   }
 </style>
