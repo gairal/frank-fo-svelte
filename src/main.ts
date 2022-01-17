de6d463656deb17a724f7213a668ef8e72a863d2
frank.gairal <frank@gairal.com>
2022-01-17 14:55:14 +0100

    fix: enable sw

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+// eslint-disable-next-line import/no-unresolved
+import { registerSW } from "virtual:pwa-register";
+
 import App from "./App.svelte";
-import "./scss/global.scss";
 import { isMapReady } from "./stores/appStore";
+import "./scss/global.scss";
+
+registerSW();
 
 // @ts-expect-error adding initMap onto the window
 window.initMap = () => isMapReady.set(true);
